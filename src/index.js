ddf2b01fa2a15706d31be7edea95532cf7f16346
Manaswini Vakiti <97661638+Manaswini2510@users.noreply.github.com>
2023-04-16 01:40:25 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React,{Component} from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class StopWatch extends Component{
+
+    state = {isTimerRunning : false, timeElapsedInSeconds : 0}
+
+    componentWillUnmount(){
+        clearInterval(this.timeInterval)
+    }
+
+    onReset= ()=>{
+        clearInterval(this.timeInterval)
+        this.setState({timeElapsedInSeconds: 0})
+    }
+
+    onStop = ()=>{
+        clearInterval(this.timeInterval)
+        this.setState({isTimerRunning : false})
+    }
+
+    updateTime = ()=>{
+        this.setState(prevState=>({
+            timeElapsedInSeconds : prevState.timeElapsedInSeconds + 1
+        }))
+    }
+
+    onStart = () =>{
+        this.timeInterval = setInterval(this.updateTime,1000)
+        this.setState({isTimerRunning : true})
+    }
+
+    renderSeconds = () =>{
+        const {timeElapsedInSeconds} = this.state
+        const outSeconds = Math.floor(timeElapsedInSeconds%60)
+        if(outSeconds<10){
+            return `0${outSeconds}`
+        }
+        return outSeconds
+    }
+    renderMinutes = () =>{
+        const {timeElapsedInSeconds} = this.state
+        const minutes = Math.floor(timeElapsedInSeconds/60)
+        if(minutes<10){
+            return `0${minutes}`
+        }
+        return minutes
+    }
+
+    render(){
+        const {isTimerRunning, timeElapsedInSeconds} = this.state
+        const time = `${this.renderMinutes()}: ${this.renderSeconds()}`
+        
+        return(
+            <div className='card'>
+                <div className='watchTime'>
+                <h1>Stopwatch</h1>
+                <div className="shadow-card">
+                    <div className='timer'>
+                        <div className='image'>
+                            <img src="https://assets.ccbp.in/frontend/react-js/stopwatch-timer.png" alt="stopwatch"/>
+                        </div>
+                        <h3>Timer</h3>
+                    </div>
+                    <h1>{time}</h1>
+                    <div className='buttons'>
+                        <button className='start' type="button" onClick={this.onStart}>Start</button>
+                        <button className='stop' type="button"onClick={this.onStop}>Stop</button>
+                        <button className='reset' type="button" onClick={this.onReset}>Reset</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default StopWatch
